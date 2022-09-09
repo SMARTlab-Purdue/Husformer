88b7054a7c4120cce9198b8be69972d6f7940074
Dezhong Zhao <52278174+zdz0086@users.noreply.github.com>
2022-09-09 21:41:23 +0800

    Update main-5.py

--- main-5.py
+++ main-5.py
@@ -62,7 +62,7 @@ parser.add_argument('--seed', type=int, default=1111,
 parser.add_argument('--no_cuda', action='store_true',
                     help='do not use cuda')
 parser.add_argument('--name', type=str, default='hus',
-                    help='name of the trial (default: "mult")')
+                    help='name of the trial (default: "hus")')
 parser.add_argument('--eval', action='store_true',
                     help='Perform evaluation only')
 args = parser.parse_args()

6d4648d9a0d2a0caf90cc88fb354c21a96560645
Dezhong Zhao <52278174+zdz0086@users.noreply.github.com>
2023-07-02 21:55:42 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -31,3 +31,26 @@ def load_model(args, name=''):
     name = save_load_name(args, name)
     model = torch.load(f'output/{args.name}.pt')
     return model
+
+
+class focalloss(nn.Module):
+    def __init__(self, alpha=[0.1, 0.1, 0.8], gamma=3, reduction='mean'):
+        super(focalloss, self).__init__()
+        self.alpha = torch.tensor(alpha)
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, pred, target):
+        target = remake_label(target).type(torch.int64)
+        alpha = self.alpha[target]
+        log_softmax = torch.log_softmax(pred, dim=1)
+        logpt = torch.gather(log_softmax, dim=1, index=target.view(-1, 1))
+        logpt = logpt.view(-1)
+        ce_loss = -logpt 
+        pt = torch.exp(logpt)
+        focal_loss = alpha * ((1 - pt) ** self.gamma * ce_loss).t()
+        if self.reduction == "mean":
+            return torch.mean(focal_loss)
+        if self.reduction == "sum":
+            return torch.sum(focal_loss)
+        return focal_loss
